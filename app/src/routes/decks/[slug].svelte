10946e247671935134b8a997be5c02b36e76be7a
Marand <marpht23@gmail.com>
2021-11-25 18:24:48 +0100

    adds next card button and fix title

--- app/src/routes/decks/[slug].svelte
+++ app/src/routes/decks/[slug].svelte
@@ -1,48 +1,58 @@
 <script context="module">
-	import deckApi from '$lib/api/decks';
+    import deckApi from '$lib/api/decks';
 
-	/** @type {import('@sveltejs/kit').Load} */
-	export async function load({ page }) {
-		return {
-			// The props are passed to the regular component script
-			props: {
-				deck: await deckApi.getBySlug(page.params.slug)
-			}
-		};
-	}
+    /** @type {import('@sveltejs/kit').Load} */
+    export async function load({page}) {
+        return {
+            // The props are passed to the regular component script
+            props: {
+                deck: await deckApi.getBySlug(page.params.slug)
+            }
+        };
+    }
 </script>
 
 <script>
-	import { fade } from 'svelte/transition';
-	// access props from the script module and expose them to the markup
-	export let deck;
+    import {fade} from 'svelte/transition';
+    // access props from the script module and expose them to the markup
+    export let deck;
+    export let currentCard = deck[0];
 
-	let showFront = true;
+    let showFront = true;
 
-	function handleFlip() {
-		console.log('handling click');
-		showFront = !showFront;
-	}
+    function handleFlip() {
+        console.log(deck)
+        console.log(currentCard)
+
+        console.log('handling click');
+        showFront = !showFront;
+    }
+
+    function next() {
+        currentCard = deck[(deck.indexOf(currentCard) + 1) % deck.length];
+        showFront = true;
+    }
 </script>
 
 <div class="max-w-3xl mx-auto w-full mt-8">
-	<div class="flex justify-center items-center h-full mt-16">
-		<div class="w-full bg-yellow-100 h-96 rounded-md">
-			{#if showFront}
-				<div transition:fade>
-					{deck[0].question}
-				</div>
-			{:else}
-				<div transition:fade>
-					{deck[0].answer}
-				</div>
-			{/if}
-		</div>
+    <div class="w-full bg-yellow-100 h-96 rounded-md p-4">
+        {#if showFront}
+            <div transition:fade>
+                {currentCard.question}
+            </div>
+        {:else}
+            <div transition:fade>
+                {currentCard.answer}
+            </div>
+        {/if}
+    </div>
 
-		<button class="bg-yellow-400 py-2.5 px-6 rounded-md" on:click={handleFlip}>Flip</button>
-	</div>
+    <div class="p-6">
+        <button class="bg-yellow-400 py-2.5 px-6 rounded-md" on:click={handleFlip}>Flip</button>
+        <button class="bg-yellow-400 py-2.5 px-6 rounded-md float-right" on:click={next}>Next</button>
+    </div>
 </div>
 
 <svelte:head>
-	<title>{deck}</title>
+    <title>{currentCard.question}</title>
 </svelte:head>
